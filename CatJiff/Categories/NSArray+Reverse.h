3c341536f726f24accd19c5793577ddf7749486d
Mattias Bergström <bergstroem1@gmail.com>
2015-02-23 22:52:47 +0100

    Add category with a method for reversing an array

---
+++ CatJiff/Categories/NSArray+Reverse.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+Reverse.h
+//  CatJiff
+//
+//  Created by Mattias Bergström on 23/02/15.
+//  Copyright (c) 2015 Mattias Bergström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Reverse)
+
+- (NSArray *)reversedArray;
+
+@end
---
+++ CatJiff/Categories/NSArray+Reverse.m
@@ -0,0 +1,23 @@
+//
+//  NSArray+Reverse.m
+//  CatJiff
+//
+//  Created by Mattias Bergström on 23/02/15.
+//  Copyright (c) 2015 Mattias Bergström. All rights reserved.
+//
+
+#import "NSArray+Reverse.h"
+
+@implementation NSArray (Reverse)
+
+- (NSArray *)reversedArray
+{
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:[self count]];
+    NSEnumerator *enumerator = [self reverseObjectEnumerator];
+    for (id element in enumerator) {
+        [array addObject:element];
+    }
+    return array;
+}
+
+@end
