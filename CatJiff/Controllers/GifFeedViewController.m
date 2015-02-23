b0f921c2a16f323e40561ca849adf8cbd13a90bc
Mattias Bergström <bergstroem1@gmail.com>
2015-02-24 00:06:57 +0100

    Adjust the tint amount

--- CatJiff/Controllers/GifFeedViewController.m
+++ CatJiff/Controllers/GifFeedViewController.m
@@ -93,7 +93,7 @@ static NSInteger const kMaxVisiblePages = 4;
         CardView *cardView = page.subviews[0];
         if (i < kMaxVisiblePages) {
             [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-                cardView.backgroundColor = [[UIColor clouds] darkenColor:0.08 * i];
+                cardView.backgroundColor = [[UIColor clouds] darkenColor:0.1 * i];
                 cardView.alpha = 1;
             } completion:nil];
         } else {
