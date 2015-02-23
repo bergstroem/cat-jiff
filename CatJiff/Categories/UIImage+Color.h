92a167cf8e250f61d82cc11922ce38ce2f2498c8
Mattias Bergström <bergstroem1@gmail.com>
2015-02-24 00:00:50 +0100

    Add category for creating uiimages from a uicolor

---
+++ CatJiff/Categories/UIImage+Color.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Color.h
+//  CatJiff
+//
+//  Created by Mattias Bergström on 23/02/15.
+//  Copyright (c) 2015 Mattias Bergström. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Color)
+
++ (UIImage *)imageWithColor:(UIColor *)color;
+
+@end
